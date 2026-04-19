947347f61c0855477dcb22414733f2d4c559ab63
henryeckersley <heckersl4210@icloud.com>
2026-04-19 14:48:47 -0400

    Create __init__.py for agents with necessary imports

---
+++ novaforge/agents/__init__.py
@@ -0,0 +1,6 @@
+from .planning_agent import PlanningAgent
+from .research_agent import ResearchAgent
+from .architecture_agent import ArchitectureAgent
+from .implementation_agent import ImplementationAgent
+from .review_agent import ReviewAgent
+from .evolution_agent import EvolutionAgent
